ec49065bdfd0423104b4f499b3b52ad3807b9bf7
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-05-10 13:37:32 +0500

    commit for js and css

--- file.js
+++ file.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
@@ -14,4 +15,11 @@ document.addEventListener('scroll', () => {
     } else {
         header.style.backgroundColor = 'transparent';
     }
+});
+
+menu_item.forEach(item=>{
+    item.addEventListener('click',()=>{
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
 });
